b3521530453e4f3ad316efbfd100a8d405ff8f04
Evgeny Polyakov <evg_pol@yahoo.com>
2022-10-15 13:40:28 +0300

    added components Board, BoardCell, GameStats, logic Board, Cell, hooks useBoard, useGameStats

---
+++ src/business/Board.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { defaultCell } from './Cell'
+
+export const buildBoard = ({ rows, columns }) => {
+    const builtRows = Array.from({ length: rows }, () =>
+        Array.from({ length: columns }, () => ({ ...defaultCell })))
+
+    return {
+        rows: builtRows,
+        size: { rows, columns }
+    }
+}
--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
+import Board from './Board'
+import GameStats from './GameStats'
+
+import { useBoard } from '../hooks/useBoard'
+import { useGameStats } from '../hooks/useGameStats'
+
+import './Tetris.css'
+
 const Tetris = ({ rows, columns, setGameOver }) => {
-    return (<p>Tetris</p>)
+    const [gameStats, addLinesCleared] = useGameStats()
+    const [board, setBoard] = useBoard({ rows, columns })
+
+    return (
+        <div className='Tetris'>
+            <Board board={board} />
+            <GameStats gameStats={gameStats} />
+        </div>
+    )
 }
 
 export default Tetris
